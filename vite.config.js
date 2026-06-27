ca57c1f4e5420281e724e366ed5e27a0c0425fcf
Diyar Gohdar Haji <142308018+Diyarhaji@users.noreply.github.com>
2026-06-27 17:39:32 +0300

    Fix: corrected errors and updated project files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react' // 👈 Fixed this import
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/diyarhaji/', // 👈 Keeps your GitHub subfolder path safe
+  base: '/', // 👈 Keeps your GitHub subfolder path safe
 })
